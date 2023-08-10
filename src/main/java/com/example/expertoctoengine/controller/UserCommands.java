81b0402c9eb1ce97919031593d236a47ae8b7b22
Denis Borisov <mai.borisov.denis@gmail.com>
2023-08-11 01:20:27 +0300

    add save password function

--- src/main/java/com/example/expertoctoengine/controller/UserCommands.java
+++ src/main/java/com/example/expertoctoengine/controller/UserCommands.java
@@ -1,7 +1,10 @@
 package com.example.expertoctoengine.controller;
 
 import static com.example.expertoctoengine.model.Commands.*;
+
+import com.example.expertoctoengine.model.Password;
 import com.example.expertoctoengine.model.Person;
+import com.example.expertoctoengine.service.PasswordService;
 import com.example.expertoctoengine.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
@@ -13,14 +16,16 @@ import org.springframework.shell.standard.ShellOption;
 public class UserCommands {
 
     private final PersonService personService;
+    private final PasswordService passwordService;
     private Person signedAccount;
 
     @Autowired
-    public UserCommands(PersonService personService) {
+    public UserCommands(PersonService personService, PasswordService passwordService) {
         this.personService = personService;
+        this.passwordService = passwordService;
     }
 
-    @ShellMethod(key = "-r", value = "Registering a new account if doest exists. Example: -r <username>")
+    @ShellMethod(key = "register", value = "Registering a new account if doest exists. Example: -r <username>")
     public String register(@ShellOption String name){
         Person newPerson = Person.builder()
                 .name(name)
@@ -34,7 +39,7 @@ public class UserCommands {
 
     }
 
-    @ShellMethod(key = "login", value = "Entering account. Example: -login <username>")
+    @ShellMethod(key = "login", value = "Enter account. Example: login <username>")
     public String login(@ShellOption String name){
         if (signedAccount == null){
             this.signedAccount = personService.getPersonByName(name);
@@ -45,7 +50,7 @@ public class UserCommands {
 
     }
 
-    @ShellMethod(key = "logout", value = "Leaving account. Example: -logout")
+    @ShellMethod(key = "logout", value = "Leaving account. Example: logout")
     public String logout(){
         if (signedAccount == null){
             return LOGOUT_ERROR.getText();
@@ -53,6 +58,22 @@ public class UserCommands {
             signedAccount = null;
             return SUCCESSFUL_LOGOUT.getText();
         }
+    }
+
+    @ShellMethod(key = "save", value = "Saving password")
+    public String savePassword(@ShellOption(arity = 3) String[] args){
+        if(signedAccount != null){
+            Password password = Password.builder().service(args[0]).login(args[1]).password(args[2]).person(signedAccount).build();
+            passwordService.savePassword(password);
+            return "Password was saved\n" +
+                    "service: " + password.getService() + "\n"+
+                    "login: " + password.getLogin() + "\n"+
+                    "password: " + password.getPassword();
+
+        }else{
+            return ACCOUNT_NOT_FOUND.getText();
+        }
+
     }
 
 }
